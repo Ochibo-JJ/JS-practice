d6e5ac5652fffefbf301c3ad7bfb39f41162a55c
Ochibo-JJ <133512040+Ochibo-JJ@users.noreply.github.com>
2023-07-09 17:38:29 +0300

    calculating hypotenuse

---
+++ index.js
@@ -0,0 +1,34 @@
+//simple way of calculating hypotenuse of a triangle
+	/*
+		let base;
+		let height;
+		let hypotenuse;
+
+		base = window.prompt("Enter base");
+		base = Number(base);
+
+		height = window.prompt("Enter height");
+		height = Number(height);
+
+		hypotenuse = Math.pow(base, 2) + Math.pow(height, 2);
+		hypotenuse = Math.sqrt(hypotenuse);
+		//alternativelt hypotenuse = Math.sqrt(Math.pow(base, 2) + Math.pow(height, 2));
+
+		console.log("The hypotenuse is:", hypotenuse)
+	*/
+
+//complex way of calculating hypotenuse of a triangle
+	let base;
+	let height;
+	let hypotenuse;
+
+	document.getElementById("submitButton").onclick = function () {
+		base = document.getElementById("aLabel").value;
+		base = Number(base);
+
+		height = document.getElementById("bLabel").value;
+		height = Number(height);
+
+		hypotenuse = Math.sqrt(Math.pow(base, 2) + Math.pow(height, 2));
+		hypotenuse = document.getElementById("cLabel").innerHTML = "Hypotenuse is:" + hypotenuse;
+	}
